0d637d49450ae04d8b62e818353ac2f05d4ae4b4
Jnanesh2005 <jnaneshmj2005@gmail.com>
2025-11-12 01:04:46 +0530

    Add non-interactive createsuperuser to build script

--- build.sh
+++ build.sh
@@ -5,4 +5,15 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py migrate#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
 python manage.py migrate
+
+# This line will run create_superuser non-interactively
+# It will only create the user the *first* time it's run
+python manage.py createsuperuser --noinput || echo "Superuser already exists."
