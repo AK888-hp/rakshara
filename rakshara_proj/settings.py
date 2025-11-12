5e774e33255ddc70a32f9b37e050248dfebf4fd6
Jnanesh2005 <jnaneshmj2005@gmail.com>
2025-11-12 19:16:42 +0530

    Switch from SMTP to SendGrid for email

--- rakshara_proj/settings.py
+++ rakshara_proj/settings.py
@@ -134,11 +134,9 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
 
-# 10. USE ENVIRONMENT VARIABLES FOR EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
+SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+# DEFAULT_FROM_EMAIL is still read from EMAIL_HOST_USER, which is correct
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
